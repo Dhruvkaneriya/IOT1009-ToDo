b5448002782a4881ce53e064b5e742dff413e359
Dhruvkaneriya <32631492+Dhruvkaneriya@users.noreply.github.com>
2022-03-18 21:56:02 -0400

    commit

---
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* Here I declared all variables */
+    Button add;
+    AlertDialog dialog;
+    LinearLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*Here I connect those variables to IDs */
+        add = findViewById(R.id.add);
+        layout = findViewById(R.id.container);
+
+        /* Here I called dialog() method) */
+        Dialog();
+
+        /* Here I make add setonclicklistner method */
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
+    }
+
+    /* Here I make Dialog() method */
+    private void Dialog() {
+
+        /* Here I make Dialog box code */
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialogbox,null);
+
+        EditText name = view.findViewById(R.id.nameEdit);
+
+        builder.setView(view);
+        builder.setTitle("Enter Fruit Name")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        addCard(name.getText().toString());
+                        Toast.makeText(MainActivity.this,"Add Fruit name Successfully!", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        dialog = builder.create();
+    }
+
+    /* Here I make addcard() method */
+    private void addCard(String name) {
+        View view = getLayoutInflater().inflate(R.layout.card, null);
+
+        TextView nameView = view.findViewById(R.id.name);
+        Button delete = view.findViewById(R.id.delete);
+
+        nameView.setText(name);
+
+        /* Here I can make add and remove fruit code */
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+                Toast.makeText(MainActivity.this,"Remove fruit name Successfully!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        layout.addView(view);
+    }
+}
